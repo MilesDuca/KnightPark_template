9c1637a3ff543d74818868c723cdd6bb4f8bdd8f
Miles <miles.duca2@gmail.com>
2022-10-21 14:15:21 -0400

    Added HTML from template and started turning them into components.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import DropDown from "./components/DropDown/DropDown";
+import Garage from "./components/Garage/Garage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container-fluid text-center">
+      <div class="top-container">
+        <div class="row">
+          <h1>Knight Park</h1>
+          <DropDown></DropDown>
+        </div>
+      </div>
+
+      <div class="middle-container">
+        <Garage></Garage>
+        <div class="row">
+          <div class="col garage"></div>
+          <div class="col garage">
+            <h2>Garage B</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <h3>Floor 1</h3>
+          </div>
+          <div class="col">
+            <h3>Floor 2</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col num-of-cars">
+            <h4>6/6</h4>
+          </div>
+          <div class="col num-of-cars">
+            <h4>3/6</h4>
+          </div>
+        </div>
+      </div>
+
+      <div class="bottom-container">
+        <p class="copyright">©Knight Park Incorporated</p>
+      </div>
     </div>
   );
 }
